985c924b6fafd9fbf23a0892653e50b20baa3944
matt <matt@atlasagency.com.au>
2020-04-28 16:50:00 +1000

    cleanups

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,13 +1,13 @@
 // https://prismic.io/docs/javascript/getting-started/integrating-with-an-existing-javascript-project
 
-import { initLayout } from './demo'
+// import { initLayout } from './demo'
 
 var Prismic = require('prismic-javascript')
 
 var apiEndpoint = 'https://broccolou.cdn.prismic.io/api/v2'
 // var apiToken = 'MC5XVHFod1NrQUFDWUE0RVFw...e-_vX5dYu-_vQnvv70'
-document.documentElement.classList.remove('no-js')
-document.documentElement.classList.add('js')
+// document.documentElement.classList.remove('no-js')
+// document.documentElement.classList.add('js')
 
 Prismic.getApi(apiEndpoint)
   .then(function(api) {
@@ -16,76 +16,76 @@ Prismic.getApi(apiEndpoint)
   .then(
     function(response) {
       console.log('Documents: ', response.results)
-      const proj = response.results //.slice(0, 5)
-      let _grid = proj
-        .map(
-          (prj, i) => `
-        <figure class="grid__item grid__item--slide">
-            <span class="number">0${(i += 1)}</span>
-            <div class="img-wrap">
-            <div
-                class="img"
-                style="background-image: url(${prj.data.project_image.url});"
-            ></div>
-            </div>
-            <figcaption class="caption">${
-              prj.data.project_name[0].text
-            }</figcaption>
-        </figure>`
-        )
-        .join(' ')
+      //   const proj = response.results //.slice(0, 5)
+      //   let _grid = proj
+      //     .map(
+      //       (prj, i) => `
+      //     <figure class="grid__item grid__item--slide">
+      //         <span class="number">0${(i += 1)}</span>
+      //         <div class="img-wrap">
+      //         <div
+      //             class="img"
+      //             style="background-image: url(${prj.data.project_image.url});"
+      //         ></div>
+      //         </div>
+      //         <figcaption class="caption">${
+      //           prj.data.project_name[0].text
+      //         }</figcaption>
+      //     </figure>`
+      //     )
+      //     .join(' ')
 
-      let titles = `<div class="titles-wrap">
-      <div class="grid grid--titles">
-        ${proj.map(
-          prj =>
-            `<h3 class="grid__item grid__item--title">${prj.project_short ||
-              'EMPTY'}</h3>`
-        )}
-      </div>
-    </div>`
+      //   let titles = `<div class="titles-wrap">
+      //   <div class="grid grid--titles">
+      //     ${proj.map(
+      //       prj =>
+      //         `<h3 class="grid__item grid__item--title">${prj.project_short ||
+      //           'EMPTY'}</h3>`
+      //     )}
+      //   </div>
+      // </div>`
 
-      const interactions = `<div class="grid grid--interaction">
-      <div class="grid__item grid__item--cursor grid__item--left"></div>
-      <div class="grid__item grid__item--cursor grid__item--center"></div>
-      <div class="grid__item grid__item--cursor grid__item--right"></div>
-    </div>`
+      //   const interactions = `<div class="grid grid--interaction">
+      //   <div class="grid__item grid__item--cursor grid__item--left"></div>
+      //   <div class="grid__item grid__item--cursor grid__item--center"></div>
+      //   <div class="grid__item grid__item--cursor grid__item--right"></div>
+      // </div>`
 
-      document.querySelector('#projects_content').innerHTML = proj
-        .map(
-          prj => `<article class="content__item">
-                <div class="img-wrap img-wrap--content">
-                <div
-                    class="img img--content"
-                    style="background-image: url(${
-                      prj.data.project_image.url
-                    });"
-                ></div>
-                </div>
-                <header class="content__item-header">
-                <span class="content__item-header-meta">${
-                  prj.data.project_name[0].text
-                }</span>
-                <h2 class="content__item-header-title">${(prj.data
-                  .project_short[0] &&
-                  prj.data.project_short[0].text) ||
-                  'Empty'}</h2>
-                </header>
-                <div class="content__item-copy">
-                <div class="content__item-copy-text">
-                ${prj.data.project_copy.map(c => `<p>${c.text}</p>`)}
-                </div>
-                <a href="#" class="content__item-copy-more">Link +</a>
-                </div>
-                </article>`
-        )
-        .join(' ')
-      //
-      document.querySelector('#projects').innerHTML =
-        _grid + titles + interactions
+      //   document.querySelector('#projects_content').innerHTML = proj
+      //     .map(
+      //       prj => `<article class="content__item">
+      //             <div class="img-wrap img-wrap--content">
+      //             <div
+      //                 class="img img--content"
+      //                 style="background-image: url(${
+      //                   prj.data.project_image.url
+      //                 });"
+      //             ></div>
+      //             </div>
+      //             <header class="content__item-header">
+      //             <span class="content__item-header-meta">${
+      //               prj.data.project_name[0].text
+      //             }</span>
+      //             <h2 class="content__item-header-title">${(prj.data
+      //               .project_short[0] &&
+      //               prj.data.project_short[0].text) ||
+      //               'Empty'}</h2>
+      //             </header>
+      //             <div class="content__item-copy">
+      //             <div class="content__item-copy-text">
+      //             ${prj.data.project_copy.map(c => `<p>${c.text}</p>`)}
+      //             </div>
+      //             <a href="#" class="content__item-copy-more">Link +</a>
+      //             </div>
+      //             </article>`
+      //     )
+      //     .join(' ')
+      //   //
+      //   document.querySelector('#projects').innerHTML =
+      //     _grid + titles + interactions
 
-      //
-      initLayout()
+      //   //
+      //   initLayout()
 
       //   document.body.classList.remove('loading')
     },
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,13 +1,15 @@
 // import { Application } from 'stimulus'
 // import Turbolinks from 'turbolinks'
 
-// import './api'
+import './api'
 
 import { initLayout } from './demo'
 
 document.addEventListener('DOMContentLoaded', function() {
   // Handler when the DOM is fully loaded
   initLayout()
+  document.documentElement.classList.toggle(['no-js'])
+  document.documentElement.classList.toggle(['js'])
 })
 
 // import HelloController from './controllers/hello_controller'
